d283118f85cbf0d9976e49290da3bae8928867ba
Hennadii Bublikov <bublikov.gennadiy@gmail.com>
2025-03-28 17:16:27 +0200

    фикст даты и структуры файла

--- controllers/dataController.js
+++ controllers/dataController.js
@@ -7,98 +7,124 @@ import ctrlWrapper from "../decorators/ctrlWrapper.js";
 const upload = multer({ dest: "uploads/" });
 
 const uploadExcel = async (req, res) => {
-  if (!req.file) {
-    return res.status(400).json({ message: "No file uploaded" });
-  }
+    if (!req.file) {
+      return res.status(400).json({ message: "No file uploaded" });
+    }
+  
+    const filePath = req.file.path;
+    const workbook = xlsx.readFile(filePath);
+    const sheetName = workbook.SheetNames[0];
+    const sheet = workbook.Sheets[sheetName];
+    const jsonData = xlsx.utils.sheet_to_json(sheet, { header: 1 }); // Получаем массив массивов
+  
+    if (jsonData.length <= 1) {
+      return res.status(400).json({ message: "No data found in the Excel file" });
+    }
+  
+    // Определение значений для столбцов
+    const orgStructureRSM = "orgStructureRSM";
+    const orgStructureTSM = "orgStructureTSM";
+    const orgStructureSUP = "orgStructureSUP";
+    const orgStructureTP = "orgStructureTP";
+    const visitDate = "visitDate";
+    const monthYear = "monthYear";
+    const ttActualName = "ttActualName";
+    const ttCity = "ttCity";
+    const ttActualAddress = "ttActualAddress";
+    const ttSubtype = "ttSubtype";
+    const ttComment = "ttComment";
+    const ttAdditionalId = "ttAdditionalId";
+    const ttNetwork = "ttNetwork";
+    const mrmMkk = "mrmMkk";
+    const ttNumber = "ttNumber";
+    const survey = "survey";
+    const surveyPage = "surveyPage";
+    const surveyElement = "surveyElement";
+    const surveyAnswer = "surveyAnswer";
+    const surveyContentLink = "surveyContentLink";
+  
+    const columnMap = {
+      "Оргструктура: RSM": orgStructureRSM,
+      "Оргструктура: TSM": orgStructureTSM,
+      "Оргструктура: SUP": orgStructureSUP,
+      "Оргструктура: ТП": orgStructureTP,
+      "Візит: Дата": visitDate,
+      "Міс'Рік": monthYear,
+      "ТТ: Фактична назва": ttActualName,
+      "Географія ТТ: Місто": ttCity,
+      "ТТ: Фактична адреса": ttActualAddress,
+      "ТТ: Підтип": ttSubtype,
+      "ТТ: Коментар (Сегмент)": ttComment,
+      "ТТ: Додатковий ідентифікатор": ttAdditionalId,
+      "ТТ: Мережа": ttNetwork,
+      "МРМ/МКК": mrmMkk,
+      "ТТ: №": ttNumber,
+      Анкета: survey,
+      "Анкета: Сторінка": surveyPage,
+      "Анкета: Елемент": surveyElement,
+      "Анкета: Відповідь": surveyAnswer,
+      "Анкета: Посилання на контент МБД": surveyContentLink,
+    };
+  
+    const groupedData = {};
 
-  const filePath = req.file.path;
-  const workbook = xlsx.readFile(filePath);
-  const sheetName = workbook.SheetNames[0];
-  const sheet = workbook.Sheets[sheetName];
-  const jsonData = xlsx.utils.sheet_to_json(sheet, { header: 1 }); // Получаем массив массивов
+  jsonData.slice(1).forEach((row) => {
+    let obj = {};
+    jsonData[0].forEach((key, index) => {
+      if (columnMap[key]) {
+        let value = row[index] || "";
 
-  if (jsonData.length <= 1) {
-    return res.status(400).json({ message: "No data found in the Excel file" });
-  }
-
-  // Определение значений для столбцов
-  const orgStructureRSM = "orgStructureRSM";
-  const orgStructureTSM = "orgStructureTSM";
-  const orgStructureSUP = "orgStructureSUP";
-  const orgStructureTP = "orgStructureTP";
-  const visitDate = "visitDate";
-  const monthYear = "monthYear";
-  const ttActualName = "ttActualName";
-  const ttCity = "ttCity";
-  const ttActualAddress = "ttActualAddress";
-  const ttSubtype = "ttSubtype";
-  const ttComment = "ttComment";
-  const ttAdditionalId = "ttAdditionalId";
-  const ttNetwork = "ttNetwork";
-  const mrmMkk = "mrmMkk";
-  const ttNumber = "ttNumber";
-  const survey = "survey";
-  const surveyPage = "surveyPage";
-  const surveyElement = "surveyElement";
-  const surveyAnswer = "surveyAnswer";
-  const surveyContentLink = "surveyContentLink";
-
-  const columnMap = {
-    "Оргструктура: RSM": orgStructureRSM,
-    "Оргструктура: TSM": orgStructureTSM,
-    "Оргструктура: SUP": orgStructureSUP,
-    "Оргструктура: ТП": orgStructureTP,
-    "Візит: Дата": visitDate,
-    "Міс'Рік": monthYear,
-    "ТТ: Фактична назва": ttActualName,
-    "Географія ТТ: Місто": ttCity,
-    "ТТ: Фактична адреса": ttActualAddress,
-    "ТТ: Підтип": ttSubtype,
-    "ТТ: Коментар (Сегмент)": ttComment,
-    "ТТ: Додатковий ідентифікатор": ttAdditionalId,
-    "ТТ: Мережа": ttNetwork,
-    "МРМ/МКК": mrmMkk,
-    "ТТ: №": ttNumber,
-    Анкета: survey,
-    "Анкета: Сторінка": surveyPage,
-    "Анкета: Елемент": surveyElement,
-    "Анкета: Відповідь": surveyAnswer,
-    "Анкета: Посилання на контент МБД": surveyContentLink,
-  };
-
-  const formattedData = jsonData
-    .slice(1)
-    .map((row) => {
-      let obj = {};
-      jsonData[0].forEach((key, index) => {
-        if (columnMap[key]) {
-          let value = row[index] || "";
-
-          // Заполняем обязательные поля значением по умолчанию, если они пустые
-          if (key === "Візит: Дата" && typeof value === "string") {
-            obj[columnMap[key]] = new Date(
-              value.split(".").reverse().join("-")
-            );
+        // Обрабатываем дату
+        if (key === "Візит: Дата" && typeof value === "string") {
+          // Попытка преобразовать значение в метку времени
+          const timestamp = new Date(value).getTime();
+          if (!isNaN(timestamp)) {
+            // Если метка времени валидна, то создаем новый объект Date
+            const date = new Date(timestamp);
+            // Форматируем дату как dd.mm.yyyy
+            value = `${String(date.getDate()).padStart(2, "0")}.${String(date.getMonth() + 1).padStart(2, "0")}.${date.getFullYear()}`;
           } else {
-            obj[columnMap[key]] = value || ""; // или какое-то значение по умолчанию
+            value = ""; // Если не удалось преобразовать, оставляем пустое значение
           }
         }
-      });
 
-      return obj;
-    })
-    .filter((item) => item !== null); // Убираем строки с ошибками из finalData
+        obj[columnMap[key]] = value;
+      }
+    });
 
-  await DataModel.deleteMany();
-  const insertedData = await DataModel.insertMany(formattedData);
+    const { visitDate, ttNumber } = obj;
+    if (!visitDate || !ttNumber) return;
 
-  // Удаляем загруженный файл (если он есть)
-  fs.unlink(filePath, (err) => {
-    if (err) console.error("Ошибка удаления файла:", err);
+    if (!groupedData[visitDate]) {
+      groupedData[visitDate] = {};
+    }
+
+    if (!groupedData[visitDate][ttNumber]) {
+      groupedData[visitDate][ttNumber] = [];
+    }
+
+    groupedData[visitDate][ttNumber].push(obj);
   });
 
-  res.json({ message: "Data uploaded successfully", data: insertedData });
-};
+  // Удаление всех данных перед вставкой новых
+  await DataModel.deleteMany();
+
+  // Вставляем данные в базу данных
+  const insertedData = await DataModel.insertMany(
+    Object.entries(groupedData).map(([visitDate, ttNumbers]) => ({
+      visitDate,
+      ttNumbers,
+    }))
+  );
+  
+    // Удаляем загруженный файл (если он есть)
+    fs.unlink(filePath, (err) => {
+      if (err) console.error("Ошибка удаления файла:", err);
+    });
+  
+    res.json({ message: "Data uploaded successfully", data: insertedData });
+  };
+  
 
 const getData = async (req, res) => {
   const filters = req.query;
--- models/data.js
+++ models/data.js
@@ -1,27 +1,40 @@
 import mongoose from "mongoose";
 
-const dataSchema = new mongoose.Schema({
-  orgStructureRSM: { type: String, required: true },
-  orgStructureTSM: { type: String, required: true },
-  orgStructureSUP: { type: String, required: true },
-  orgStructureTP: { type: String, required: true },
-  visitDate: { type: Date, required: true },
-  monthYear: { type: String, required: true },
-  ttActualName: { type: String, required: true },
-  ttCity: { type: String, required: true },
-  ttActualAddress: { type: String, required: true },
-  ttSubtype: { type: String, required: true },
-  ttComment: { type: String },
-  ttAdditionalId: { type: String },
-  ttNetwork: { type: String },
-  mrmMkk: { type: String },
-  ttNumber: { type: String, required: true },
-  survey: { type: String },
-  surveyPage: { type: String},
-  surveyElement: { type: String },
-  surveyAnswer: { type: String },
-  surveyContentLink: { type: String},
-}, { timestamps: true });
+
+const dataSchema = new mongoose.Schema(
+  {
+    visitDate: { type: String, required: true },
+    ttNumbers: {
+      type: Map,
+      of: [
+        new mongoose.Schema(
+          {
+            orgStructureRSM: { type: String, required: true },
+            orgStructureTSM: { type: String, required: true },
+            orgStructureSUP: { type: String, required: true },
+            orgStructureTP: { type: String, required: true },
+            monthYear: { type: String },
+            ttActualName: { type: String, required: true },
+            ttCity: { type: String, required: true },
+            ttActualAddress: { type: String, required: true },
+            ttSubtype: { type: String, required: true },
+            ttComment: { type: String },
+            ttAdditionalId: { type: String },
+            ttNetwork: { type: String },
+            mrmMkk: { type: String },
+            survey: { type: String },
+            surveyPage: { type: String },
+            surveyElement: { type: String },
+            surveyAnswer: { type: String },
+            surveyContentLink: { type: String },
+          },
+          { _id: false }
+        ),
+      ],
+    },
+  },
+  { timestamps: true }
+);
 
 const DataModel = mongoose.model("Data", dataSchema);
 export default DataModel;
